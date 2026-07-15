47da64a1e1905bb35e9468d4e1acbbea7efef64d
Athithya1617 <athithya1617@gmail.com>
2026-07-15 16:47:57 +0530

    Configure React Router basename for GitHub Pages subfolder

--- AgriGrowFrountend/src/App.jsx
+++ AgriGrowFrountend/src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="app-container">
           <Navbar />
           <main className="main-content">
